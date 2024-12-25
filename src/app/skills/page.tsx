b27374714ceafd7c64b07005527c7ed72b745f4e
syedaaleezaabbas <syedaaleezaabbas@gmail.com>
2024-12-25 15:20:39 +0500

    push portfolio-css to github

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,12 @@
+import Skills from '@/components/Skills'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <Skills />
+        </div>
+    )
+}
+
+export default page

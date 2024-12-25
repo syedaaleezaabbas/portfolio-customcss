ec83bbc5ac79b8896c50608ed7ee8a41a255e837
syedaaleezaabbas <syedaaleezaabbas@gmail.com>
2024-12-25 08:06:16 +0500

    add skills section to portfolio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
       <div className={styles.textSection}>
         <h1 data-aos="fade-up">About Me</h1>
         <p data-aos="fade-up">
-          Hi! I'm Aleeza, a dedicated Frontend Web Developer focused on creating clean, intuitive, and visually engaging websites. I’m passionate about blending creativity with technical expertise to deliver responsive designs and exceptional user experiences. Continuously striving to learn and evolve, I aim to stay at the forefront of web development by exploring new tools and techniques. Whether developing from the ground up or enhancing existing designs, I’m committed to bringing innovative ideas to life with precision and purpose.
+          Hi! {"I'm"} Aleeza, a dedicated Frontend Web Developer focused on creating clean, intuitive, and visually engaging websites. {"I’m"} passionate about blending creativity with technical expertise to deliver responsive designs and exceptional user experiences. Continuously striving to learn and evolve, I aim to stay at the forefront of web development by exploring new tools and techniques. Whether developing from the ground up or enhancing existing designs, {"I’m"} committed to bringing innovative ideas to life with precision and purpose.
         </p>
       </div>
     </div>
